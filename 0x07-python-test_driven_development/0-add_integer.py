ceb946548df2d59c8fc300cced5dc0b67624fd53
Dongmo-tsapgo Brendoom <cocbrandon25@gmail.com>
2023-02-02 20:49:18 -0800

    o redeone

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,3 +1,11 @@
+#!/usr/bin/python3
+"""
+Module add-integer
+Adds two integer together
+
+"""
+
+
 def add_integer(a, b=98):
     """Returns the addition of a and b,
     or an error if a and b are not integers or floats
