e49f2101803c7192f55dee881d9b2cf447544171
Dongmo-tsapgo Brendoom <cocbrandon25@gmail.com>
2023-02-02 20:53:58 -0800

    100 redone

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -6,7 +6,8 @@ Multiplies two matrices and returns the result.
 
 def matrix_mul(m_a, m_b):
     """Return the matrix resulting of
-    the multiplication of m_a and m_b."""
+    the multiplication of m_a and m_b.
+    """
 
     if type(m_a) is not list:
         raise TypeError("m_a must be a list")
